bc065a6fbdd8a25856c9badc0652d55cbdd7d87c
Karan Gupta <karanguptak.g4567@gmail.com>
2025-03-21 21:37:20 +0530

    [Karan Gupta] Added : Validation on User input Name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'HelloWorld';
-  imgUrl="../assets/BL_logo.jpg";
-  url="https://www.bridgelabz.com";
-  userName: string = "";
+  imgUrl = '../assets/BL_logo.jpg';
+  url = 'https://www.bridgelabz.com';
+  userName: string = '';
+  userError: string = '';
 
-  OnClick($event : Event){  
-    console.log("Save button is clicked",$event);
-    window.open(this.url,"_blank");
+  OnClick($event: Event) {
+    console.log('Save button is clicked', $event);
+    window.open(this.url, '_blank');
+  }
+
+  onInput() {
+    console.log('Change Event Occurred', this.userName);
+    const nameRegex = /^[A-Za-z]{2,}(?: [A-Za-z]+)*$/; // Allows spaces but no numbers/special chars
+    this.userError = nameRegex.test(this.userName) ? '' : 'Name is Incorrect';
   }
 }
